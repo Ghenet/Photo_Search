7f5a8b7eaad08185024cc68c9b398c80ac171279
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2021-11-22 09:35:07 -0800

    adds all containers and form element to the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
-
+import SearchPhotosForm from './SearchPhotosForm';
 function App() {
   return (
     <div className="App">
-     hello
+      <div className="container">
+        <h1 className="title">Photo Search</h1>
+        <SearchPhotosForm />
+      </div>
     </div>
   );
 }
 
 export default App;
+
