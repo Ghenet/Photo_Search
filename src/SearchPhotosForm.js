9223badef9caf688a98317562d45cb6d15d93dd5
Ghenet Andemichael <38365838+Ghenet@users.noreply.github.com>
2021-11-22 10:58:06 -0800

    display the images by mapping over the elements in pics

--- src/SearchPhotosForm.js
+++ src/SearchPhotosForm.js
@@ -36,6 +36,18 @@ export default function SearchPhotosForm (){
                         />
                     <button type="submit" className="button">Search</button>
                 </form>
+                <div className="card-list">
+                    {pics.map((pic) =><div className="card" key={pic.id}> 
+                                    <img 
+                                        className="card--image"
+                                        alt={pic.alt_description}
+                                        src={pic.urls.full}
+                                        width="50%"
+                                        height="50%"
+                                        />
+                                    
+                                    </div>)}
+                </div>
             </div>
         )
     }
